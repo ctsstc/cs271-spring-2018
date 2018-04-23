cec049fced13b60f09a64c424a6c22b9123b2d2f
Cody Swartz <ctsstc@gmail.com>
2018-04-22 17:54:16 -0700

    The library way
    Uses bitset to quickly take an input and

--- swartzco_hw3.cpp
+++ swartzco_hw3.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <stdlib.h>
+#include <bitset>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    cout << "Enter a number between 0 and 65535." << endl;
+
+    cout << ">> ";
+    int number;
+    cin >> number;
+    cout << "Your Input " << number << endl;
+
+    string binary = bitset<16>(number).to_string(); //to binary
+    cout << binary << endl;
+
+}
+void validateInput()
+{
+
+}
+
+string toBinary(uint num)
+{
+    return "";
 }
