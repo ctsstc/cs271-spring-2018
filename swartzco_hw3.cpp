d64ef2902debdd21691dcad63a17c47baf1d9b27
Cody Swartz <ctsstc@gmail.com>
2018-04-22 18:05:28 -0700

    Nice input flow now with error checking loop

--- swartzco_hw3.cpp
+++ swartzco_hw3.cpp
@@ -4,23 +4,44 @@
 
 using namespace std;
 
+int getInput()
+{
+    int number;
+    // HUD
+    cout << "Enter a number between 0 and 65535." << endl;
+    cout << ">> ";
+    // Input
+    cin >> number;
+    cout << "Your Input: " << number << endl;
+    return number;
+}
+
+bool validateInput(int num)
+{
+    bool invalid = num < 0 || num > UINT16_MAX;
+    if (invalid)
+    {
+        cout << endl << "The number you entered: " << num 
+             << " is less than 0 or greather than " 
+             << UINT16_MAX << endl;
+    }
+    return !invalid;
+}
+
 int main()
 {
-    cout << "Enter a number between 0 and 65535." << endl;
-
-    cout << ">> ";
     int number;
-    cin >> number;
-    cout << "Your Input " << number << endl;
+
+    // Nag user for proper input
+    do {
+        number = getInput();
+    } while(!validateInput(number));
 
     string binary = bitset<16>(number).to_string(); //to binary
-    cout << binary << endl;
+    cout << "Binary: " << binary << endl;
 
 }
-void validateInput()
-{
 
-}
 
 string toBinary(uint num)
 {
