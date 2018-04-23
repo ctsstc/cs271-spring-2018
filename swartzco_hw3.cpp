d3495ad2d5c5daae778b28cba09822b53dd3d86b
Cody Swartz <ctsstc@gmail.com>
2018-04-22 19:22:05 -0700

    All snazzed up and ready to go!

--- swartzco_hw3.cpp
+++ swartzco_hw3.cpp
@@ -6,44 +6,77 @@ using namespace std;
 
 int getInput()
 {
-    int number;
+    string input;
     // HUD
-    cout << "Enter a number between 0 and 65535." << endl;
+    cout << "Enter a number between 0 and 65535. Or x to exit." << endl;
     cout << ">> ";
     // Input
-    cin >> number;
-    cout << "Your Input: " << number << endl;
-    return number;
+    cin >> input;
+    // Exit?
+    if (input == "x" || input == "exit")
+    {
+        exit(0);
+    }
+    cout << "Your Input: " << input << endl;
+    int num;
+    try {
+        num = stoi(input);
+    }
+    catch(invalid_argument &e) {
+        num = -1;
+    }
+    return num;
 }
 
-bool validateInput(int num)
+bool validInput(int num)
 {
-    bool invalid = num < 0 || num > UINT16_MAX;
-    if (invalid)
+    bool valid = !(num < 0 || num > UINT16_MAX);
+    if (!valid)
     {
         cout << endl << "The number you entered: " << num 
              << " is less than 0 or greather than " 
              << UINT16_MAX << endl;
     }
-    return !invalid;
+    return valid;
+}
+
+string toBinary(uint num)
+{
+    uint start = (UINT16_MAX / 2) + 1;
+    string sb = "";
+
+    do
+    {
+        if (num >= start)
+        {
+            sb += "1";
+            num -= start;
+        }
+        else
+        {
+            sb += "0";
+        }
+        //cout << "NUM: " << num << " START: " << start << " BIN: " << sb << endl ;
+        start /= 2;
+    } while (start != 0);
+
+    return sb;
 }
 
 int main()
 {
     int number;
 
-    // Nag user for proper input
     do {
-        number = getInput();
-    } while(!validateInput(number));
+        // Nag user for proper input
+        do {
+            number = getInput();
+        } while(!validInput(number));
 
-    string binary = bitset<16>(number).to_string(); //to binary
-    cout << "Binary: " << binary << endl;
+        // Cheater's way ;) #verify
+        string binary = bitset<16>(number).to_string(); //to binary
+        cout << "Binary cheat: " << binary << endl;
+        cout << "Binary legit: " << toBinary(number) << endl << endl;
+    } while(true);
 
-}
-
-
-string toBinary(uint num)
-{
-    return "";
 }
